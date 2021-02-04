6cc0f809fd140ab70ec9e8e34842c194ded8d2ba
Benjamin Smith <70050256+Bsmi3275@users.noreply.github.com>
2021-02-04 06:57:48 -0500

    Add files via upload
    Outside files

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-
 //Express part/port
 const app = express ();
 var PORT = process.env.PORT || 8080;
 
-
 const db = require("./models");
 const HTMLroutes = require("./routes/html");
 const APIroutes = require("./require/APIroutes");
-
 
 //Express hookup with everything else 
 app.use(express.urlencoded({ extended: true }));
@@ -25,16 +22,15 @@ app.use(express.static("public"));
 app.use(logger("dev"));
 
 //calling the routes
-app.use(require("./routes/HTMLroutes"));
-app.use(require("./require/APIroutes"));
-
+app.use(require("./routes/html"));
+app.use(require("./require/api"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-} )
+});
 
 //PORT function
 app.listen(PORT, function () {
